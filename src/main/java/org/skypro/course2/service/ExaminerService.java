e74c7c503949280f9bed97b51bb31398bbf3b5d2
Randell87 <randell@mail.ru>
2025-12-02 11:42:12 +0700

    work

--- src/main/java/org/skypro/course2/service/ExaminerService.java
+++
@@ -1,9 +0,0 @@
-package org.skypro.course2.service;
-
-import org.skypro.course2.domain.Question;
-
-import java.util.Collection;
-
-public interface ExaminerService {
-    Collection<Question> getQuestions(int amount);
-}
